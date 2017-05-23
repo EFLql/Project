7daaa9d73ec177e7fcc141382146b97de2eb1f10
pubxtsf5 <pubxtsf5@hikvision.com>
2017-05-23 20:13:35 +0800

    [mod]提交LineBasedFrameDecoder部分实现代码

---
+++ libext/bootstrap/codec/LineBasedFrameDecoder.cpp
@@ -0,0 +1,42 @@
+#include <libext/boostrap/codec/LineBasedFrameDecoder.h>
+
+namespace libext
+{
+explicit LineBasedFrameDecoder::LineBasedFrameDecoder(
+    uint32_t maxLength, 
+    bool stripDelimiter, 
+    TerminatorType terminatorType)
+: maxLength_(maxLength)
+, stripDelimiter_(stripDelimiter_)
+, terminatorType_(terminatorType)
+{
+}
+
+bool LineBasedFrameDecoder::decode(Context* ctx, 
+                                   libext::IOBufQueue& buf, 
+                                   std::unique_ptr<IOBuf>& result, 
+                                   size_t& needed)
+{
+    int64_t eol = findEndOfLine(buf);
+    if(!discarding_)
+    {
+        if(eol > maxLength_)
+        {
+
+        }
+    }
+}
+
+int64_t LineBasedFrameDecoder::findEndOfLine(libext::IOBufQueue& buf)
+{
+    
+}
+
+void LineBasedFrameDecoder::fail(Context* ctx, std::string len)
+{
+
+}
+
+
+
+}
--- libext/bootstrap/codec/LineBasedFrameDecoder.h
+++ libext/bootstrap/codec/LineBasedFrameDecoder.h
@@ -1,14 +1,39 @@
 #pragma once
 #include <libext/bootstrap/codec/ByteToMessageDecoder.h>
+#include <string>
 
 namespace libext
 {
 class LineBasedFrameDecoder : public ByteToByteDecoder
 {
 public:
-    LineBasedFrameDecoder();
-    ~LineBasedFrameDecoder() {}
-    bool decode(Context* ctx, IOBufQueue& buf, IOBuf& result, size_t& needed); 
+    enum class TerminatorType
+    {
+        BOTH,
+        NEWLINE,
+        CARRIGENEWLINE,
+    };
+    explicit LineBasedFrameDecoder(
+        uint32_t maxLength = UINT_MAX,
+        bool stripDelimiter = true,
+        TerminatorType terminatorType = TerminatorType::BOTH);
+ 
+    bool decode(Context* ctx, 
+                libext::IOBufQueue& buf, 
+                std::unique_ptr<IOBuf>& result, 
+                size_t& needed);
+
+private:
+    int64_t findEndOfLine(libext::IOBufQueue& buf);
+    void fail(Context* ctx, std::string len);
+
+    uint32_t maxLength_;
+    bool stripDelimiter_{true};
+
+    bool discarding_{false};
+    uint32_t discardedBytes_{0};
+
+    TerminatorType terminatorType_;
 };
 
 }
